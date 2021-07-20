a4248625d1895652ebc8bc91008acf1efdfe8fa5
Stefan Velickovic <velickolb@gmail.com>
2021-07-20 13:56:35 +0200

    Started working on screwing script.

---
+++ Assets/Scripts/Screw.cs
@@ -0,0 +1,37 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class Screw : MonoBehaviour
+    {
+        public VRTK_InteractableObject linkedObject;
+
+        protected virtual void OnEnable()
+        {
+            linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+
+            if (linkedObject != null)
+            {
+                linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+            }
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (linkedObject != null)
+            {
+                linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+            }
+        }
+
+        protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+        {
+            Screwing();
+        }
+
+        protected virtual void Screwing()
+        {
+            print("SCREWING");
+        }
+    }
+}
