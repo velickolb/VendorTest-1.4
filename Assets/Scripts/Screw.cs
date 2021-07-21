9decce2aa50c43fcf747cfea34356cef3cb8ffc5
Stefan Velickovic <velickolb@gmail.com>
2021-07-21 19:37:21 +0200

    Added hose model. Added events for connecting hose.

--- Assets/Scripts/Screw.cs
+++ Assets/Scripts/Screw.cs
@@ -3,6 +3,7 @@
     using System;
     using UnityEngine;
     using UnityEngine.Animations;
+    using UnityEngine.Events;
 
     public class Screw : MonoBehaviour
     {
@@ -11,15 +12,40 @@
         public Transform head;
 
         public float spinSpeed = 180f;
-
-        protected bool spinning;
+        /// <summary>
+        /// Is drill spinning
+        /// </summary>
+        protected bool isSpinning;
+        /// <summary>
+        /// Is hose connected to drill
+        /// </summary>
+        protected bool IsConnected = false;
 
         public GameObject bit;
         public GameObject target;
-
+        /// <summary>
+        /// Sound to play on hose connect
+        /// </summary>
         public AudioClip ConnectSound;
-
+        /// <summary>
+        /// Point for attaching the hose
+        /// </summary>
         public Transform HoseAttackPoint;
+        /// <summary>
+        /// Fired on screw reaching limit
+        /// </summary>
+        UnityEvent OnScrewed;
+
+
+        /// <summary>
+        /// Info canvases
+        /// </summary>
+        public GameObject FinishedBoard,StartingBoard;
+
+        /// <summary>
+        /// Warning canvases
+        /// </summary>
+        public GameObject HoseNotConnected;
 
         protected virtual void OnEnable()
         {
@@ -33,12 +59,15 @@
                 dropZone.ObjectSnappedToDropZone += ConnectHose;
             }
 
+            if (OnScrewed == null)
+                OnScrewed = new UnityEvent();
+
+            OnScrewed.AddListener(Screwed);
+
         }
 
-        private Action<SnapDropZoneEventArgs> ParentHose()
-        {
-            throw new NotImplementedException();
-        }
+
+
 
         protected virtual void OnDisable()
         {
@@ -54,26 +83,33 @@
 
         protected virtual void Update()
         {
-            if (spinning)
+            if (isSpinning)
             {
                 head.transform.Rotate(new Vector3(-1 * spinSpeed * Time.deltaTime,0f,0f));
 
-                if(bit.GetComponent<SphereCollider>().bounds.Contains(target.transform.position) && target.transform.position.y > 0.5935f)
+                if (bit.GetComponent<SphereCollider>().bounds.Contains(target.transform.position) && target.transform.position.y > 0.8000f)
                 {
-                    target.transform.Rotate(new Vector3(0f,spinSpeed * Time.deltaTime, 0f));
-                    target.transform.Translate(new Vector3(0f,Time.deltaTime*-0.01f, 0f));
+                    target.transform.Rotate(new Vector3(0f, spinSpeed * Time.deltaTime, 0f));
+                    target.transform.Translate(new Vector3(0f, Time.deltaTime * -0.01f, 0f));
+                }
+                else if (target.transform.position.y < 0.8000f)
+                {
+                    OnScrewed.Invoke();
                 }
                 else
                 {
-                    Debug.Log("NOT SCREWING");
+                    Debug.Log("Not screwing");
                 }
 
             }
         }
 
         protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
-        {
-            StartScrew();
+        {   
+            if (IsConnected)
+                StartScrew();
+            else
+                HoseNotConnected.SetActive(true);
         }
 
         protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
@@ -82,20 +118,29 @@
         }
 
         protected virtual void StartScrew()
-        {   
-            spinning = true;
+        {
+            isSpinning = true;
         }
 
         protected virtual void StopScrew()
         {
-            spinning = false;
+            isSpinning = false;
         }
 
         protected virtual void ConnectHose(object sender, SnapDropZoneEventArgs e)
         {   
             HoseAttackPoint.SetParent(dropZone.transform.parent.transform);
-
+            HoseAttackPoint.Rotate(new Vector3(-90,0,0));
+            IsConnected = true;
+            HoseNotConnected.SetActive(true);
             print(dropZone.transform.parent.transform.name);
         }
+
+        private void Screwed()
+        {
+            FinishedBoard.SetActive(true);
+            StartingBoard.SetActive(false);
+        }
+
     }
 }
