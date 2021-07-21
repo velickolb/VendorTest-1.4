6bf188254a1969a99077d9d5c2dd4a7479c134f2
Stefan Velickovic <velickolb@gmail.com>
2021-07-21 08:53:51 +0200

    Added lookAt.

--- Assets/Scripts/Screw.cs
+++ Assets/Scripts/Screw.cs
@@ -1,22 +1,27 @@
 ﻿namespace VRTK.Examples
 {
     using UnityEngine;
+    using UnityEngine.Animations;
 
     public class Screw : MonoBehaviour
     {
         public VRTK_InteractableObject linkedObject;
         public Transform head;
 
-        public float spinSpeed = 360f;
+        public float spinSpeed = 180f;
 
         protected bool spinning;
 
         public GameObject bit;
         public GameObject target;
 
+        LookAtConstraint lookAt;
+
         protected virtual void OnEnable()
         {
             linkedObject = (linkedObject == null ? GetComponent<VRTK_InteractableObject>() : linkedObject);
+            
+            lookAt = (lookAt == null ? GetComponent<LookAtConstraint>() : lookAt);
 
             if (linkedObject != null)
             {
@@ -43,11 +48,15 @@
                 if(bit.GetComponent<SphereCollider>().bounds.Contains(target.transform.position))
                 {
                     target.transform.Rotate(new Vector3(0f,-1 * spinSpeed * Time.deltaTime, 0f));
-                    target.transform.Translate(new Vector3(0f,Time.deltaTime*-0.05f, 0f));
+                    target.transform.Translate(new Vector3(0f,Time.deltaTime*-0.01f, 0f));
+
+                    lookAt.constraintActive = true;
+
                 }
                 else
                 {
                     Debug.Log(" NOT SCREWING");
+                    lookAt.constraintActive = false;
                 }
 
             }
@@ -61,6 +70,7 @@
         protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
         {
             StopScrew();
+            lookAt.constraintActive = false;
         }
 
         protected virtual void StartScrew()
